62fbbf2634683a950df73d3046793fc84e3cb599
Oscar Miranda <oimirandaa08@gmail.com>
2023-10-07 05:09:41 -0600

    Added home page

---
+++ src/components/Home/index.js
@@ -0,0 +1,46 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import {Link} from "react-router-dom";
+import './index.scss';
+import {useEffect, useState} from "react";
+import AnimatedLetters from "../AnimatedLetters";
+const Home = () =>{
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['l', 'o','b','o','d','a','n']
+    const jobArray = ['f', 'u', 'l', 'l', 's', 't', 'a', 'c', 'k', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+    }, [])
+
+    return (
+        <>
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br/>
+                    <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m,</span>
+
+                    <img src={LogoTitle} alt="developer"/>
+                    <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={15}/>
+                    <br/>
+                    <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={25}/>
+                    <br/>
+                </h1>
+
+                <h2> Full Stack Developer / Java Expert / Dog dad</h2>
+
+                <Link to="/contact" className="flat-button">
+                    CONTACT ME
+                </Link>
+            </div>
+        </div>
+        </>
+    )
+}
+
+export default Home;
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,7 +4,7 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faEnvelope, faHome, faUser} from "@fortawesome/free-solid-svg-icons";
-import {faDev, faGithub, faLinkedin, faTelegram, faTwitter} from "@fortawesome/free-brands-svg-icons";
+import {faDev, faGithub, faLinkedin, faTwitter} from "@fortawesome/free-brands-svg-icons";
 const Sidebar = () => (
     <div className ='nav-bar'>
         <Link className='logo' to='/'>
